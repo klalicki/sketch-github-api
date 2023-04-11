776a35e157407471332c2ac3c9e86b210da2bf12
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-04-11 23:34:31 +0000

    adds code to pull all projects from a user and generates basic tiles for them

--- scripts.js
+++ scripts.js
@@ -2,19 +2,45 @@ import { Octokit } from "https://cdn.skypack.dev/octokit";
 export const octokit = new Octokit({});
 window.octokit = octokit; // for debugging
 
-const getRepos = (username) => {
+// gets all repos from a user, and filters them by ones that contain a specific topic.
+// adds the filtered repos to the DOM
+const getRepos = (username, topicFilter) => {
   const repoList = octokit
     .request(`GET /users/${username}/repos`)
     .then((response) => {
       console.log(response.data);
       // filter by repos that include topic 'sketches'
-      const sketchRepos = response.data.filter((repo) =>
-        repo.topics.includes("sketches")
-      );
-      console.log(sketchRepos);
-      sketchRepos.forEach((repo) => {
+      const targetRepos = response.data.filter((repo) => {
+        if (!topicFilter) {
+          return true;
+        } else {
+          return repo.topics.includes(topicFilter);
+        }
+      });
+      console.log(targetRepos);
+      const cleanedTargetRepos = [];
+      targetRepos.forEach((repo) => {
         const { name, description, html_url, homepage } = repo;
-        console.log({name, description, html_url, homepage});
+        cleanedTargetRepos.push({ name, description, html_url, homepage });
+      });
+      const sketchListElem = document.querySelector(".sketch-list");
+      cleanedTargetRepos.forEach((repo) => {
+        const newElem = document.createElement("article");
+        const header = document.createElement("h3");
+        header.innerText = repo.name;
+        const description = document.createElement("p");
+        description.innerText = repo.description;
+        const repoLink = document.createElement("a");
+        repoLink.innerText = "View Repo";
+        repoLink.href = repo.html_url;
+        const sketchLink = document.createElement("a");
+        sketchLink.innerText = "View Sketch";
+        sketchLink.href = repo.homepage;
+        newElem.appendChild(header);
+        newElem.appendChild(description);
+        newElem.appendChild(repoLink);
+        newElem.appendChild(sketchLink);
+        sketchListElem.appendChild(newElem);
       });
     });
 };
